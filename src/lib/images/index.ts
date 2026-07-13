ec0cc62e36907e7398ee331c60c35027b4663658
Alexandros Mannari <alex.mannari@tapaas.com>
2026-07-13 18:16:47 +0300

    updated invitation image

--- src/lib/images/index.ts
+++ src/lib/images/index.ts
@@ -1,5 +1,3 @@
-export { default as invitationImage } from './invitation.jpg'
-export { default as invitationImage01 } from './INVITATION-01.jpg'
 export { default as invitationImage02 } from './INVITATION-02.jpg'
 export { default as waxSeal } from './was_seal.png'
 
